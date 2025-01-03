2932d4dbd1684eb65de2d5214174b921a9f9fbc0
slnnzmtl <slonanezametil@gmail.com>
2025-01-03 12:58:21 +0700

    feat: add directory index files for components
    - Add icons/index.ts for icon exports\n- Add feedback/index.ts for feedback component exports\n- Add campaign/index.ts for campaign component exports

---
+++ frontend/src/components/feedback/index.ts
@@ -0,0 +1,5 @@
+export { default as ErrorPage } from './ErrorPage';
+export { default as MetaMaskError } from './MetaMaskError';
+export { default as EmptyState } from './EmptyState';
+export { default as CampaignDetailsPlaceholder } from './CampaignDetailsPlaceholder';
+export { default as CampaignListPlaceholder } from './CampaignListPlaceholder'; 
