60469f5fd552c1b23aeb26301d7e2fdf3dfc4be9
slnnzmtl <slonanezametil@gmail.com>
2025-01-16 16:01:51 +0700

    feat: Update app-level network handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import { ErrorProvider } from "./hooks/useError";
 import { ModalProvider } from "./hooks/useModal";
 import { useWalletEvents } from "./hooks/useWalletEvents";
-import NavigationMenu from "./components/ui/NavigationMenu";
 import Modals from "./components/modals";
 import "./styles/globals.css";
 import Router from "./Router";
 
 function App() {
   useWalletEvents();
-
-  const isAuthenticated = false;
 
   return (
     <ErrorProvider>
